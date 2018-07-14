575fbd93dc5550f15156c7643c54fefffea79b83
MinhduongUNO <35475533+MinhduongUNO@users.noreply.github.com>
2018-07-14 00:42:22 -0500

    Update settings.py

--- efs/settings.py
+++ efs/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'efs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Minhduong$default',
+        'USER': 'Minhduong',
+        'PASSWORD': 'Lovemylife243?',
+        'HOST': 'Minhduong.mysql.pythonanywhere-services.com',
     }
 }
 
